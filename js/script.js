2730f4be185b51e67c92f4e9cd2b2cd984bde208
azima8888 <110724271+azima8888@users.noreply.github.com>
2022-10-25 14:00:01 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var seyahet = [
+    ["Tebiet",
+            ["Deniz","Hava limani"],"Ismayilli","Almaniya"
+        ],
+    ["Aytac","Gultac","Sema","Nubar","Azima-Esmira"],
+    ["RevaN","Sebuhi","Rza","Deyanet"],
+    [
+        "Shah dag",["Yay","Qis","Mandarin"]
+    ],
+    ["","Baki."],
+    ["2","0"],
+    ["4"]
+
+]
+console.log("Mənim adım "+seyahet[0][3][0]+seyahet[2][2][1]+seyahet[3][1][1][1]+seyahet[1][2][2]+seyahet[1][4][11]+"dır."+"Təvəllüdüm "+seyahet[5][0]+seyahet[5][1]+seyahet[5][1]+seyahet[6]+"-dür")
